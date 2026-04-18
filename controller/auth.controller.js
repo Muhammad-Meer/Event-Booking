45c129e82c5cce61df75cb0319edde86b245fa15
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-18 13:47:03 +0500

    newcode

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,50 +1,51 @@
-const usermodel = require('../models/user.model');
-const bcrypt = require('bcryptjs')
+const registeruser = async (req, res) => {
+  const { fullname, email, password } = req.body;
 
+  try {
+    // check user exist
+    const isuseralreadyExist = await usermodel.findOne({ email });
 
-const registeruser = async (req , res) => {
-    const {fullname , email , password} = req.body
-    
-    try {
-      const isuseralreadyExist = await usermodel.findOne({
-        email
-      })
-
-      if(isuseralreadyExist) {
-        return res.status(400).json({
-          message: "user is already exist"
-        })
-
-        if(password.lenght < 6) {
-          return res.status(400).json({
-            message: "password must be at least 6 characters"
-          })
-
-
-          const passwordHash = await bcrypt.hash(password , 10);
-
-          const newuser = new usermodel({
-            fullname,
-            email,
-            password: passwordHash
-          })
-        
-        }
-      }
-    } catch (error) {
-      
+    if (isuseralreadyExist) {
+      return res.status(400).json({
+        message: "user already exists"
+      });
     }
-}
 
-const loginuser = async (req , res) => {
+    // password validation
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "password must be at least 6 characters"
+      });
+    }
 
-}
+    // hash password
+    const passwordHash = await bcrypt.hash(password, 10);
 
-const logout = async (req , res) => {
+    // create user
+    const newuser = await usermodel.create({
+      fullname,
+      email,
+      password: passwordHash
+    });
 
-}
+const token = jwt.sign(
+  { id: newuser._id },
+  process.env.SECRET_KEY,
+  { expiresIn: "3d" }
+);
 
-      
-module.exports = {registeruser, loginuser, }
+// cookie set karo
+res.cookie("token", token);
 
- 
+res.status(201).json({
+  message: "user registered successfully",
+  user: newuser,
+  token
+});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "internal server error"
+    });
+  }
+};
--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const authroute = require('../routes/auth.route')
+const cookieparser = require('cookie-parser');
 
 
 const app = express();
-app.use(express.json());
 
+
+app.use(express.json());
+app.use(cookieparser());
 app.use(authroute)
 
 
