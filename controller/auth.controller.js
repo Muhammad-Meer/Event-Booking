9410edb92d1e8fb347f10f33265cc21c185917fb
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-18 10:57:32 +0500

    newcode

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,15 +1,19 @@
 const usermodel = require('../models/user.model');
 
 
-
- 
 const registeruser = async (req , res) => {
     const {name , email , password} = req.body
 
     try {
       const user = await usermodel.findOne({
-
+       
+        email
       })
+      if(user) {
+        return res.status(400).json({
+          message: "user is already exist"
+        })
+      }
     } catch (error) {
       
     }
