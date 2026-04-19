ea923b5ed2cdac8887ff56b56ef75e71bbc75aa1
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-19 15:21:57 +0500

    newcode

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,6 +1,27 @@
 const foodpartnermodel = require('../models/foodpartner.model');
+const jwt = require('jsonwebtoken');
 
 
 const authmiddleware = (req, res, enxt) => {
 
+  const token = req.cookies.token;
+
+  if(!token) {
+    return res.status(401).json({
+      message: "unauthorized please login first"
+    })
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+     const foodpartner = foodpartnermodel.findById(decoded.id);
+
+     req.foodpartner = foodpartner
+
+  } catch (error) {
+    res.status(401).json({
+      message: "invalid token"
+    })
+  }
 }
