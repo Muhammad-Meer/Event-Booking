ebcff873c8e811c67b11afad10a180c8bb9f6a03
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-20 17:47:36 +0500

    newcode

---
+++ services/storage.service.js
@@ -0,0 +1,11 @@
+const imagekit = require('@imagekit/nodejs')
+
+
+
+const imagekit = new ImageKit({
+
+  IMAGEKIT_PUBLIC_KEY: IMAGEKIT_PUBLIC_KEY,
+    IMAGEKIT_PRIVATE_KEY: IMAGEKIT_PRIVATE_KEY,
+  IMAGEKIT_ENDPOINT: IMAGEKIT_ENDPOINT
+
+})
